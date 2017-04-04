39cba7d523654f7e28751873084ebc6d6725219e
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2017-04-04 17:00:19 -0300

    Adição de parametros
    Permite modificar a cor e a normalização

--- hist3_function.py
+++ hist3_function.py
@@ -4,7 +4,7 @@ Created on Mon Mar 20 22:54:21 2017
 
 @author: Rafael
 
-V 1.0.1
+V 1.0.2
 """
 
 import numpy as np
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 
 
-def hist3(x,binx,biny):
+def hist3(x,bins = [10,10], normed = False, color = 'blue'):
     
     import numpy as np
     import matplotlib.pyplot as plt
@@ -22,19 +22,20 @@ def hist3(x,binx,biny):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
-    H, xedges = np.histogramdd(x, bins = (binx,biny))
+    
+    H, xedges = np.histogramdd(x, bins, normed = normed)
 
     H = H.T
 
-    X = np.array(list(np.linspace(min(xedges[0]),max(xedges[0]),binx))*binx)     #np.array(list(np.linspace(min(x[0]),max(x[0]),binx))*binx)
-    Y = np.sort(list(np.linspace(min(xedges[1]),max(xedges[1]),biny))*biny)      #np.sort(list(np.linspace(min(x[1]),max(x[1]),biny))*biny)
+    X = np.array(list(np.linspace(min(xedges[0]),max(xedges[0]),bins[0]))*bins[0])     #np.array(list(np.linspace(min(x[0]),max(x[0]),binx))*binx)
+    Y = np.sort(list(np.linspace(min(xedges[1]),max(xedges[1]),bins[1]))*bins[1])      #np.sort(list(np.linspace(min(x[1]),max(x[1]),biny))*biny)
     dz = np.array([]);
 
-    for i in range(binx):
-        for j in range(biny):
+    for i in range(bins[0]):
+        for j in range(bins[1]):
             dz = np.append(dz, H[i][j])
     
-    Z = np.zeros(binx*biny)
+    Z = np.zeros(bins[0]*bins[1])
 
     #dx = np.linspace(min(x[0]),max(x[0]),binx)
     dx = xedges[0][1] - xedges[0][0]
@@ -42,8 +43,10 @@ def hist3(x,binx,biny):
     #dy = np.linspace(min(x[1]),max(x[1]),biny)
     dy = xedges[1][1] - xedges[1][0]
 
-    ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5)
-
+    
+    ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5, color = color)
+    
+    
 
 def hist32(x,y,binx,biny):
     
@@ -102,8 +105,8 @@ def hist32(x,y,binx,biny):
     
 ##################################
 
-mux = 1
-sigmax = 2
+mux = 4
+sigmax = 7
 
 muy = 0
 sigmay = 5
@@ -116,7 +119,7 @@ y = np.random.normal(muy,sigmay,(events,2));
 
 
 
-hist3(x,10,10)
+hist3(x,[10,10])
 
 
 

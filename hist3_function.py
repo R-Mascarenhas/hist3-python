8c488f2490321b4b97d84fc3dd8d584bdf659114
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2017-04-07 16:44:35 -0300

    Hold On
    enables hold on func in the plot

--- hist3_function.py
+++ hist3_function.py
@@ -4,7 +4,7 @@ Created on Mon Mar 20 22:54:21 2017
 
 @author: Rafael
 
-V 1.1.1
+V 1.1.2
 """
 
 import numpy as np
@@ -12,20 +12,24 @@ import matplotlib.pyplot as plt
 
 
 
-def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, *args,**kwargs):
+def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, hold = False):
     
     import numpy as np
     import matplotlib.pyplot as plt
     import pylab
     from mpl_toolkits.mplot3d import Axes3D
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    
+    
+        
     
     if np.size(bins) == 1:
         bins = [bins,bins]
     
-    H, edges = np.histogramdd(x, bins = bins, normed = normed)
+    if(len(x) == 2):
+        x = x.T;
+        
+    
+    H, edges = np.histogramdd(x, bins, normed = normed)
 
     H = H.T
     X = np.array(list(np.linspace(min(edges[0]),max(edges[0]),bins[0]))*bins[1])   
@@ -44,9 +48,33 @@ def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, *args,**kwargs
     
     
     dy = Y[bins[0]] - Y[0]
-
     
-    ax.bar3d(X,Y,Z,dx,dy,dz, alpha = alpha, color = color);
+   
+    
+    if (not hold):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.bar3d(X,Y,Z,dx,dy,dz, alpha = alpha, color = color);
+    else:
+        try:
+            ax = plt.gca();
+            ax.bar3d(X,Y,Z,dx,dy,dz, alpha = alpha, color = color);
+        except:
+            plt.close(plt.get_fignums()[-1])
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.bar3d(X,Y,Z,dx,dy,dz, alpha = alpha, color = color);
+            
+        
+        
+    
+#==============================================================================
+#         except:
+#             fig = plt.figure()
+#             ax = fig.add_subplot(111, projection='3d')
+#             ax.bar3d(X,Y,Z,dx,dy,dz, alpha = alpha, color = color);
+#==============================================================================
+            
     plt.xlabel('X');
     plt.ylabel('Y');
     
@@ -57,59 +85,61 @@ def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, *args,**kwargs
 
 
 
-def hist32(x,y,binx,biny):
-    
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import pylab
-    from mpl_toolkits.mplot3d import Axes3D
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-    bins = (binx,biny)
-    
-    H, edges = np.histogramdd(x, bins, normed = False)
-    H = H.T
-
-    X = list(np.linspace(min(x[0]),max(x[0]),binx))*binx
-    Y = np.sort(list(np.linspace(min(x[1]),max(x[1]),biny))*biny)
-    dz = []
-
-    for i in range(binx):
-        for j in range(biny):
-            dz.append(H[i][j])
-    
-    Z = np.zeros(binx**2)
-
-    dx = X[1] - X[0]
-    dy = Y[biny]-Y[0]
-
-    ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5)
-    
-    ###########################################################
-    
-    H, edges = np.histogramdd(y, bins, normed = True)
-
-    H = H.T
-
-    X = np.array(list(np.linspace(min(y[0]),max(y[0]),binx))*binx)
-    Y = np.sort(list(np.linspace(min(y[1]),max(y[1]),biny))*biny)
-    dz = np.array([]);
-
-    for i in range(binx):
-        for j in range(biny):
-            dz= np.append(dz, H[i][j])
-    
-    Z = np.zeros(binx**2)
-
-    
-    dx = X[1] - X[0]
-    dy = Y[biny]-Y[0]
-
-    ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5, color = 'g')
-    
-    plt.show()
+#==============================================================================
+# def hist32(x,y,binx,biny):
+#     
+#     import numpy as np
+#     import matplotlib.pyplot as plt
+#     import pylab
+#     from mpl_toolkits.mplot3d import Axes3D
+# 
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+#     
+#     bins = (binx,biny)
+#     
+#     H, edges = np.histogramdd(x, bins, normed = False)
+#     H = H.T
+# 
+#     X = list(np.linspace(min(x[0]),max(x[0]),binx))*binx
+#     Y = np.sort(list(np.linspace(min(x[1]),max(x[1]),biny))*biny)
+#     dz = []
+# 
+#     for i in range(binx):
+#         for j in range(biny):
+#             dz.append(H[i][j])
+#     
+#     Z = np.zeros(binx**2)
+# 
+#     dx = X[1] - X[0]
+#     dy = Y[biny]-Y[0]
+# 
+#     ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5)
+#     
+#     ###########################################################
+#     
+#     H, edges = np.histogramdd(y, bins, normed = True)
+# 
+#     H = H.T
+# 
+#     X = np.array(list(np.linspace(min(y[0]),max(y[0]),binx))*binx)
+#     Y = np.sort(list(np.linspace(min(y[1]),max(y[1]),biny))*biny)
+#     dz = np.array([]);
+# 
+#     for i in range(binx):
+#         for j in range(biny):
+#             dz= np.append(dz, H[i][j])
+#     
+#     Z = np.zeros(binx**2)
+# 
+#     
+#     dx = X[1] - X[0]
+#     dy = Y[biny]-Y[0]
+# 
+#     ax.bar3d(X,Y,Z,dx,dy,dz, alpha = 0.5, color = 'g')
+#     
+#     plt.show()
+#==============================================================================
     
     
 ##################################
@@ -117,21 +147,18 @@ def hist32(x,y,binx,biny):
 mux = 4
 sigmax = 7
 
-muy = 0
+muy = 6
 sigmay = 5
 
 events = 1000
 
 #x = np.random.randn(events,2)
 x = np.random.randn(events,2)*sigmax + mux;
-y = np.random.normal(muy,sigmay,(events,2));
+y = np.random.normal(muy,sigmay,(2,events));
 
 
 
-hist3(x,[50,20], normed = True)
-
-
-
-
+hist3(x,[10,10], normed = True, hold = False, alpha = 0.3)
+hist3(y,[10,10], normed = True, hold = True, color = 'g', alpha = 0.5)
 
 

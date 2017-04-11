8b7df2d569b08e9ce056059a6bb7eb922bda6cc2
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2017-04-11 20:54:32 -0300

    Renamed to histogram3d function

--- hist3_function.py
+++ hist3_function.py
@@ -7,20 +7,59 @@ Created on Mon Mar 20 22:54:21 2017
 V 1.1.2
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-
-def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, hold = False):
+def histogram3d(x,bins = 10, normed = False, color = 'blue', alpha = 1, hold = False):
     
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import pylab
-    from mpl_toolkits.mplot3d import Axes3D
+    """
     
+    Plotting a 3D histogram
+    Parameters
+    ----------
     
+    sample : array_like.		
+        The data to be histogrammed. It must be an (N,2) array or data 
+        that can be converted to such. The rows of the resulting array 
+        are the coordinates of points in a 2 dimensional polytope.
+    
+    bins : sequence or int, optional, default: 10.
+        The bin specification:
         
+        * A sequence of arrays describing the bin edges along each dimension.
+        * The number of bins for each dimension (bins =[binx,biny])
+        * The number of bins for all dimensions (bins = bins).
+    
+    normed : bool, optional, default: False.
+        If False, returns the number of samples in each bin. 
+        If True, returns the bin density bin_count / sample_count / bin_volume.
+    
+    color: string, matplotlib color arg, default = 'blue'
+    
+    alpha: float, optional, default: 1.
+        0.0 transparent through 1.0 opaque
+    
+    hold: boolean, optional, default: False
+    
+    Returns   
+    --------
+    H : ndarray.
+        The bidimensional histogram of sample x.
+    
+    edges : list.
+        A list of 2 arrays describing the bin edges for each dimension.
+        
+    See Also 
+    --------
+    histogram: 1-D histogram
+    histogram2d: 2-D histogram
+    histogramdd: N-D histogram
+    
+    Examples
+    --------
+    >>> r = np.random.randn(1000,2)
+    >>> H, edges = np.histogram3d(r,bins=[10,15])
+    """
+    
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
     
     if np.size(bins) == 1:
         bins = [bins,bins]
@@ -43,13 +82,8 @@ def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, hold = False):
     
     Z = np.zeros(bins[0]*bins[1])
 
-   
-    dx = X[1] - X[0]
-    
-    
+    dx = X[1] - X[0]   
     dy = Y[bins[0]] - Y[0]
-    
-   
     
     if (not hold):
         fig = plt.figure()
@@ -70,28 +104,6 @@ def hist3(x,bins = 10, normed = False, color = 'blue', alpha = 1, hold = False):
     plt.ylabel('Y');
     
     edges = [X,Y];
-    hist = dz.reshape(bins[0],bins[1]);
+    H = dz.reshape(bins[0],bins[1]);
     
-    return hist, edges
-#==============================================================================
-# 
-# 
-# mux = 4
-# sigmax = 7
-# 
-# muy = 6
-# sigmay = 5
-# 
-# events = 1000
-# 
-# x = np.random.randn(events,2)*sigmax + mux;
-# y = np.random.normal(muy,sigmay,(2,events));
-# 
-# 
-# 
-# hist3(x,[10,10], normed = True, hold = False, alpha = 0.3)
-# hist3(y,[10,10], normed = True, hold = True, color = 'g', alpha = 0.5)
-# 
-# 
-# 
-#==============================================================================
+    return H, edges;
